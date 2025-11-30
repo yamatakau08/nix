65559a0998b1d2e73d7d8f13bb2e44be0fc22df5
yamatakau08 <yamatakau08@gmail.com>
2025-11-30 21:13:43 +0900

    Add NixOS flake

---
+++ nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    xremap-flake.url = "github:xremap/nix-flake";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      tnt = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+  	  ./configuration.nix
+        ];
+	specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
