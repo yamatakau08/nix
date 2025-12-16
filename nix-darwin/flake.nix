9bb0537b492a4ae4e7e59c79e4a5b4970cb0dbad
yamatakau08 <yamatakau08@gmail.com>
2025-12-16 14:07:43 +0900

    merge home-manager into nix-darwin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,9 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, mac-app-util }:
     let
       hostnames = {
         roswell = {
@@ -16,6 +21,7 @@
         };
       };
 
+      # nix-darwin システム設定
       configuration = systemConfig: { pkgs, username, ... }: {
         # List packages installed in system profile. To search by name, run:
         # $ nix-env -qaP | grep wget
@@ -39,16 +45,45 @@
         # The platform the configuration will be used on.
         nixpkgs.hostPlatform = systemConfig.hostPlatform;
 
+        # Allow unfree packages
+        nixpkgs.config.allowUnfree = true;
+
         # when execute `sudo darwin-rebuild switch --flake ~/.config/nix-darwin#roswell`,
         # the following error appears.
         # error: Determinate detected, aborting activation
         # Determinate uses its own daemon to manage the Nix installation that
-        # conflicts with nix-darwin’s native Nix management.
-        # To turn off nix-darwin’s management of the Nix installation, set:
+        # conflicts with nix-darwin's native Nix management.
+        # To turn off nix-darwin's management of the Nix installation, set:
         nix.enable = false;
+
+        # home-manager configuration
+        users.users.${username} = {
+          home = "/Users/${username}";
+        };
       };
 
-      # Darwin設定を生成する関数
+      # home-manager 設定を生成（nix-darwin と統合）
+      mkHomeManagerModule = systemConfig:
+        let
+          system = systemConfig.hostPlatform;
+          pkgs = nixpkgs.legacyPackages.${system};
+          isDarwin = pkgs.stdenv.isDarwin;
+          username = systemConfig.username;
+          homeDirectory = if isDarwin then "/Users/${username}" else "/home/${username}";
+        in
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {
+            inherit username homeDirectory isDarwin;
+          };
+          home-manager.users.${username} = import ../home-manager/home.nix;
+          home-manager.sharedModules = [
+            mac-app-util.homeManagerModules.default
+          ];
+        };
+
+      # Darwin 設定を生成
       mkDarwinConfiguration = hostname: systemConfig:
         nix-darwin.lib.darwinSystem {
           specialArgs = {
@@ -59,6 +94,8 @@
             ./fonts.nix
             ./fish.nix
             ./finder.nix
+            home-manager.darwinModules.home-manager
+            (mkHomeManagerModule systemConfig)
           ];
         };
   in
