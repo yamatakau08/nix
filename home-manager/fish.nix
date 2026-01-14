2f8795c217597f3216a06b11442b957ccbc5ad44
yamatakau08 <yamatakau08@gmail.com>
2026-01-14 23:15:35 +0900

    Fix: Handles Darwin before executing `brew shellenv`

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -4,10 +4,12 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      if test "$(uname -m)" = "arm64"
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      else
-        eval "$(/usr/local/bin/brew shellenv)"
+      if test "$(uname -s)" = "Darwin"
+        if test "$(uname -m)" = "arm64"
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        else
+          eval "$(/usr/local/bin/brew shellenv)" # x86_64 intel Mac
+        end
       end
 
       if type -q direnv
