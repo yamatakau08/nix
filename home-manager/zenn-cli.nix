968f0c9f9e7c5fc9dcacc1b6d854a2639f28f20d
yamatakau08 <yamatakau08@gmail.com>
2026-02-16 21:49:48 +0900

    update zenn-cli to v0.4.5.

--- home-manager/zenn-cli.nix
+++ home-manager/zenn-cli.nix
@@ -4,19 +4,19 @@
   home.packages = with pkgs; [
     (zenn-cli.overrideAttrs (oldAttrs: rec {
       pname = "zenn-cli";
-      version = "0.2.10";
+      version = "0.4.5";
 
       src = pkgs.fetchFromGitHub {
         owner = "zenn-dev";
         repo = "zenn-editor";
-        rev = "zenn-cli@${version}";
-        hash = "sha256-wItKDLAJHIyxUUaLIFM+sNYWtXKWC4P6GkCKn2Wh2JA=";
+        rev = "v${version}";
+        hash = "sha256-hX+bb6eEHzzWKhmXkHoaM6197rVhMFG8vO6nJ2+1bVY=";
       };
 
-      pnpmDeps = pkgs.pnpm_9.fetchDeps {
+      pnpmDeps = pkgs.pnpm_10.fetchDeps {
         inherit pname version src;
-        hash = "sha256-WXsS5/J08n/dWV5MbyX4vK7j1mfiUoLdzwmzyqoX3FA=";
-        fetcherVersion = 2;  # これを追加
+        hash = "sha256-hCseIc9TPRdGTDvL7M5ewrvgb/jj+QPNvd1lR/DwCgc=";
+        fetcherVersion = 2; # pnpm の依存関係取得方法のバージョン 2:新しい方式
       };
     }))
   ];
