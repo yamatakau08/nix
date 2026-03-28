cc69c1d9f1fb857c8fbac4a18dac65e4ff7f1079
yamatakau08 <yamatakau08@gmail.com>
2026-03-28 19:20:19 +0900

    fix the error on rebuild switch

--- home-manager/direnv.nix
+++ home-manager/direnv.nix
@@ -5,5 +5,15 @@
     enable = true;
     enableBashIntegration = true;
     nix-direnv.enable = true;
+
+    package = pkgs.direnv.overrideAttrs (oldAttrs: {
+      env = builtins.removeAttrs (oldAttrs.env or { }) [ "CGO_ENABLED" ];
+
+      postPatch = (oldAttrs.postPatch or "") + ''
+        sed -i '/ifeq ($(shell uname), Darwin)/,/^endif/{/linkmode=external/d}' GNUmakefile
+      '';
+
+      doCheck = false;
+    });
   };
 }
