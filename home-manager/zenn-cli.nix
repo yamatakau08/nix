b3a15daaca81ec1c6bd8c855bbc6de853ca4c2ce
yamatakau08 <yamatakau08@gmail.com>
2026-07-12 21:09:23 +0900

    update zenn-cli to 0.5.2

--- home-manager/zenn-cli.nix
+++ home-manager/zenn-cli.nix
@@ -4,18 +4,18 @@
   home.packages = with pkgs; [
     (zenn-cli.overrideAttrs (oldAttrs: rec {
       pname = "zenn-cli";
-      version = "0.4.5";
+      version = "0.5.2";
 
       src = pkgs.fetchFromGitHub {
         owner = "zenn-dev";
         repo = "zenn-editor";
         rev = "v${version}";
-        hash = "sha256-hX+bb6eEHzzWKhmXkHoaM6197rVhMFG8vO6nJ2+1bVY=";
+        hash = "sha256-zvQ8b7ZaDBYGMGqOcttixjaQpC27vtvZ6ZXpzmg9Sf4=";
       };
 
       pnpmDeps = pkgs.pnpm_10.fetchDeps {
         inherit pname version src;
-        hash = "sha256-hCseIc9TPRdGTDvL7M5ewrvgb/jj+QPNvd1lR/DwCgc=";
+        hash = "sha256-tPA2pgbvcFIL3UXr+G6XLdmdTxzdx6pi6KnzvvkCYAk=";
         fetcherVersion = 3;
       };
     }))
