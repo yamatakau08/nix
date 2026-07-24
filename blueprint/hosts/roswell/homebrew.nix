009924840805bafe8d7522543d7efb20489db90c
yama <yama@roswell.local>
2026-07-24 21:44:54 +0900

    add hammerspoon via casks, delete karabiner-elements

--- blueprint/hosts/roswell/homebrew.nix
+++ blueprint/hosts/roswell/homebrew.nix
@@ -18,13 +18,16 @@
     ];
 
     casks = [
-      "karabiner-elements"
+      "hammerspoon"
+      # "karabiner-elements"
       ## Claude Desktop is registered with auto_update: true
       ## Since upgrade is skipped, add greedy = true;
       { name = "claude"; greedy = true;} # Desktop
     ];
 
     masApps = {
+      ## Since LINE install failed, comment the following
+      # "LINE" = 539883307;
       ## Since mas (Mac App Store command line interface) can't find DiXiM Play U, comment the following
       # "DiXiM Play U" = 1576816161;
     };
