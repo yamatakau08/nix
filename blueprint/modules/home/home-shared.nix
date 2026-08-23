4488df1390a486c9feb77f8a785b645969c53ac1
yamatakau08 <yamatakau08@gmail.com>
2026-08-24 08:55:13 +0900

    Changed android-file-transfer to be installed only on Linux
    When copying a directory to an Android device via Finder,
    the operation does not perform a proper recursive copy.
    Additionally, when overwriting an existing directory on the Android side,
    the existing files inside that directory are removed.
    
    To avoid these issues, the workflow on macOS has been switched back to using
    Android File Transfer for directory copy operations.

--- blueprint/modules/home/home-shared.nix
+++ blueprint/modules/home/home-shared.nix
@@ -18,7 +18,6 @@
     ./gnupg.nix
     ./wezterm.nix
     ./obsidian.nix
-    ./android-file-transfer.nix
     ./google-chrome.nix
     ./git.nix
 
@@ -29,6 +28,7 @@
   ++ lib.optionals osConfig.nixpkgs.hostPlatform.isLinux [
     ./anki.nix
     ./scrcpy.nix
+    ./android-file-transfer.nix
   ]
   ++ lib.optionals osConfig.nixpkgs.hostPlatform.isDarwin [
   ];
