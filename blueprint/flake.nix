fa729f251c745bdd54d09d35c58791df2763b06e
yamatakau08 <yamatakau08@gmail.com>
2026-09-10 18:43:27 +0900

    add nixpkgs unstable entry in defalut.nix and update flake.lock

--- blueprint/flake.nix
+++ blueprint/flake.nix
@@ -32,7 +32,6 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs-25_11";
     };
-
   };
 
   # Load the blueprint
--- blueprint/hosts/nixos/default.nix
+++ blueprint/hosts/nixos/default.nix
@@ -2,13 +2,29 @@
 {
   class = "nixos";
 
-  value = inputs.nixpkgs-25_11.lib.nixosSystem {
+  # value = inputs.nixpkgs.lib.nixosSystem { # system packages unstable
+  #   system = "x86_64-linux";
+  #   specialArgs = { inherit inputs flake; };
+  #   modules = [
+  #     ./configuration.nix
+  #     inputs.nixos-hardware.nixosModules.apple-t2
+  #     inputs.home-manager.nixosModules.home-manager # home-manager: unstable
+  #     {
+  #       home-manager.useGlobalPkgs = true;
+  #       home-manager.useUserPackages = true;
+  #       home-manager.users.yama = import ./users/yama/home-configuration.nix;
+  #       home-manager.extraSpecialArgs = { inherit inputs; };
+  #     }
+  #   ];
+  # };
+
+  value = inputs.nixpkgs-25_11.lib.nixosSystem { # system packages 25.11
     system = "x86_64-linux";
     specialArgs = { inherit inputs flake; };
     modules = [
       ./configuration.nix
       inputs.nixos-hardware.nixosModules.apple-t2
-      inputs.home-manager-25_11.nixosModules.home-manager
+      inputs.home-manager-25_11.nixosModules.home-manager # home-manager packages 25.11
       {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
